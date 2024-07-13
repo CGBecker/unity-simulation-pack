85f5bea68fa18fb6b323a76a2d058075549a758d
CGBecker <israel.becker23@hotmail.com>
2024-07-13 16:04:49 +0100

    Added some prototype behaviour

--- Scripts/Controllers/FourWheelController.cs
+++ Scripts/Controllers/FourWheelController.cs
@@ -4,15 +4,69 @@ using UnityEngine;
 
 public class FourWheelController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public ArticulationBody RootArticulationBody;
+    public ArticulationBody[] WheelsMotors;
 
-    // Update is called once per frame
-    void Update()
+    public ArticulationBody[] WheelsSuspensions;
+
+    public ArticulationBody[] WheelsSteering;
+    public float MaxSteeringForce;
+    private float steeringForce;
+
+    public ArticulationBody[] WheelsWithBrakes;
+
+
+
+    void Start() 
     {
-        
+        for (int wheelsSteeringIndex = 0; wheelsSteeringIndex < WheelsSteering.Length; wheelsSteeringIndex++)
+            {
+                WheelsSteering[wheelsSteeringIndex].SetDriveForceLimit(ArticulationDriveAxis.Y, MaxSteeringForce);
+                Debug.Log("Setting maximum steering force: " + WheelsSteering[wheelsSteeringIndex].transform.name
+                            + " to " + MaxSteeringForce);
+            }
+    }
+    
+    void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            for (int motorsIndex = 0; motorsIndex < WheelsMotors.Length; motorsIndex++)
+            {
+                WheelsMotors[motorsIndex].AddRelativeTorque(Vector3.forward * 1000);
+                Debug.Log("Trying to add torque to: " + WheelsMotors[motorsIndex].transform.name);
+            }
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            for (int brakesIndex = 0; brakesIndex < WheelsWithBrakes.Length; brakesIndex++)
+            {
+                WheelsWithBrakes[brakesIndex].AddRelativeTorque(Vector3.back * 1000);
+                Debug.Log("Trying to brake: " + WheelsWithBrakes[brakesIndex].transform.name);
+            }
+        }
+
+        if (Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.D))
+        {
+            steeringForce = 100f;
+        }
+        else if (Input.GetKey(KeyCode.D) && !Input.GetKey(KeyCode.A))
+        {
+            steeringForce = -100f;
+        }
+        else
+        {
+            steeringForce = 0f;
+        }
+
+        if (steeringForce != 0f)
+        {
+            for (int wheelsSteeringIndex = 0; wheelsSteeringIndex < WheelsSteering.Length; wheelsSteeringIndex++)
+            {
+                WheelsSteering[wheelsSteeringIndex].AddRelativeTorque(new Vector3(0, steeringForce, 0));
+                Debug.Log("Trying to add steer: " + WheelsSteering[wheelsSteeringIndex].transform.name);
+            }
+        }
     }
 }
