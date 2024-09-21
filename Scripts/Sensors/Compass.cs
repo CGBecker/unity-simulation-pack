426b462424387d99534781cbec4613e7038c9c41
CGBecker <israel.becker23@hotmail.com>
2024-09-21 14:33:02 +0100

    #8 - Added basic displacement sensor

---
+++ Scripts/Sensors/Compass.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Compass : BaseSensor
+{
+    // Take direction of North as input, if not assume X as North
+    public override void InitialiseSensor()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override float[] TakeReading()
+    {
+        throw new System.NotImplementedException();
+    }
+    public override float TakeReading(uint index)
+    {
+        throw new System.NotImplementedException();
+    }
+}
--- Scripts/Sensors/Displacement.cs
+++ Scripts/Sensors/Displacement.cs
@@ -4,19 +4,51 @@ using UnityEngine;
 
 public class Displacement : BaseSensor
 {
-    // command if on-demand take reading
+    private Vector3[] previousPositions;
+    public Transform[] DisplacementObjects;
+
+    /// <summary>
+    /// Initialise Displacement Sensor
+    /// WARNING: DisplacementObjects variable must be assigned before initialisation
+    /// </summary>
     public override void InitialiseSensor()
     {
-        throw new System.NotImplementedException();
+        previousPositions = new Vector3[DisplacementObjects.Length];
+        for (int i = 0; i < DisplacementObjects.Length; i++)
+        {
+            previousPositions[i] = DisplacementObjects[i].position;
+        }
     }
 
-    // take reading, on-demand or always running
+    /// <summary>
+    /// Take reading of displacement sensor array
+    /// </summary>
+    /// <returns>Array of floats with distances of displacement per sensor in meters</returns>
     public override float[] TakeReading()
     {
-        throw new System.NotImplementedException();
+        float[] displacementsInMeters = new float[DisplacementObjects.Length];
+        Vector3 tempPose;
+        for (int i = 0; i < DisplacementObjects.Length; i++)
+        {
+            tempPose = DisplacementObjects[i].position - previousPositions[i];
+            displacementsInMeters[i] = tempPose.magnitude;
+            previousPositions[i] = DisplacementObjects[i].position;
+        }
+
+        return displacementsInMeters;
     }
+
+    /// <summary>
+    /// Take reading of single displacement sensor at <paramref name="index"/>
+    /// </summary>
+    /// <param name="index"></param>
+    /// <returns>A float with the distance of displacement in meters</returns>
     public override float TakeReading(uint index)
     {
-        throw new System.NotImplementedException();
+        Vector3 tempPose = DisplacementObjects[index].position - previousPositions[index];
+        float displacementsInMeters = tempPose.magnitude;
+        previousPositions[index] = DisplacementObjects[index].position;
+
+        return displacementsInMeters;
     }
 }
