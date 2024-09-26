08634c45e8c8b42177716315826bd19c36153b9b
CGBecker <israel.becker23@hotmail.com>
2024-09-26 13:41:10 +0100

    #9 Added remaining variable checks

--- Scripts/Sensors/CameraDevice.cs
+++ Scripts/Sensors/CameraDevice.cs
@@ -62,7 +62,6 @@ public class CameraDevice : BaseSensor
             return;
         }
 
-        // Start setting up the camera configs from input with Defaults to avoid bugs but keep gameobject or component disabled
         if (CameraResolution.x <= 0 || CameraResolution.y <= 0)
         {
             Debug.LogWarning("Broken value detected for Camera Device Resolution: " + CameraResolution + ". Setting default (512,512)");
@@ -83,35 +82,55 @@ public class CameraDevice : BaseSensor
             Debug.LogWarning("Broken value detected for Camera Device FieldOfView: " + FieldOfView + ". Setting default (100)");
             FieldOfView = 100;
         }
-        // public float2 SensorSize;
-        // public uint ISO;
-        // public float ShutterSpeed;
-        // public Camera.GateFitMode GateFitMode;
-        // public float FocalLength;
-        // public float2 Shift;
-        // public float Aperture;
-        // public float FocusDistance;
-        // public uint ApertureBladeCount;
-        // public float2 ApertureCurvature;
-        // public float ApertureBarrelClipping;
-        // public float ApertureAnamorphism;
-        // public HDAdditionalCameraData.AntialiasingMode PostAntiAliasing;
-        // public bool StopNaNs;
-        // public bool Dithering;
-        // public CameraSettings.Culling Culling;
-        // public Transform ExposureTargetTransform;
-        // public CameraClearFlags BackgroundType;
-
-        // private Volume volume;
-        // private VolumeProfile volumeProfile;
-        // public ExposureMode ExposureModeSettings;
-        // public Bloom BloomSettings;
-        // public ColorAdjustments ColorAdjustmentsSettings;
-        // public FilmGrain FilmGrainSettings;
-        // public MotionBlur MotionBlurSettings;
-        // public Tonemapping TonemappingSettings;
-        // public Vignette VignetteSettings;
-
+        if (SensorSize.x < 0.1f || SensorSize.y < 0.1f)
+        {
+            Debug.LogWarning("Broken value detected for Camera Device SensorSize: " + SensorSize + ". Setting default (70,50)");
+            SensorSize = new float2(70,50);
+        }
+        if (ISO < 1)
+        {
+            Debug.LogWarning("Broken value detected for Camera Device ISO: " + ISO + ". Setting default (200)");
+            ISO = 200;
+        }
+        if (ShutterSpeed <= 0f)
+        {
+            Debug.LogWarning("Broken value detected for Camera Device ShutterSpeed: " + ShutterSpeed + ". Setting default (0.005)");
+            ShutterSpeed = 0.005f;
+        }
+        if (FocalLength < 0.2296482f)
+        {
+            Debug.LogWarning("Broken value detected for Camera Device FocalLength: " + FocalLength + ". Setting default (20)");
+        }
+        if (Aperture < 0.7f || Aperture > 32f)
+        {
+            Debug.LogWarning("Broken value detected for Camera Device Aperture: " + Aperture + ". Setting default (16)");
+            Aperture = 16f;
+        }
+        if (FocusDistance < 0.1f)
+        {
+            Debug.LogWarning("Broken value detected for Camera Device FocusDistance: " + FocusDistance + ". Setting default (10)");
+            FocusDistance = 10f;
+        }
+        if (ApertureBladeCount < 3 || ApertureBladeCount > 11)
+        {
+            Debug.LogWarning("Broken value detected for Camera Device ApertureBladeCount: " + ApertureBladeCount + ". Setting default (5)");
+            ApertureBladeCount = 5;
+        }
+        if (ApertureCurvature.x < 0.7f || ApertureCurvature.x > 32 || ApertureCurvature.y < 0.7f || ApertureCurvature.y > 32f)
+        {
+            Debug.LogWarning("Broken value detected for Camera Device ApertureCurvature: " + ApertureCurvature + ". Setting default (2,11)");
+            ApertureCurvature = new float2(2,11);
+        }
+        if (ApertureBarrelClipping < 0f || ApertureBarrelClipping > 1f)
+        {
+            Debug.LogWarning("Broken value detected for Camera Device ApertureBarrelClipping: " + ApertureBarrelClipping + ". Setting default (0.25)");
+            ApertureBarrelClipping = 0.25f;
+        }
+        if (ApertureAnamorphism < -1f || ApertureAnamorphism > 1f)
+        {
+            Debug.LogWarning("Broken value detected for Camera Device ApertureAnamorphism: " + ApertureAnamorphism + ". Setting default (0)");
+            ApertureAnamorphism = 0f;
+        }
 
         cameras = new Camera[CamerasGameobjects.Length];
         camerasDatas = new HDAdditionalCameraData[CamerasGameobjects.Length];
