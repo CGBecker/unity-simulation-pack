ddfafe5524205ff49a0f42a745bb8fd976d514af
CGBecker <israel.becker23@hotmail.com>
2024-10-01 15:21:54 +0100

    #21 Added partial configuration

--- Scripts/Actuators/ArticulationDevice.cs
+++ Scripts/Actuators/ArticulationDevice.cs
@@ -8,8 +8,8 @@ public class ArticulationDevice : BaseActuator
     /// The main parent of ALL articulation bodies for this Robot/Vehicle
     /// </summary>
     public GameObject AbsoluteParent;
-    public GameObject[] childrenArticulationBodies;
-    private ArticulationBody[] articulationBodies;
+    public GameObject[] ChildrenArticulationBodies;
+    private ArticulationBody[] _articulationBodies;
     public float[] MassPerArticulationBody;
     public bool[] UseGravity;
     public bool[] AutomaticCenterOfGravity;
@@ -17,28 +17,17 @@ public class ArticulationDevice : BaseActuator
     public bool[] Immovable;
     public float[] LinearDamping;
     public float[] AngularDamping;
-    /// <summary>
-    /// 1 - Discrete;
-    /// 2 - Continuous;
-    /// 3 - Continuous Dynamic;
-    /// 4 - Continuous Speculative;
-    /// </summary>
-    public uint[] CollisionDetection;
+    public CollisionDetectionMode[] CollisionDetectionModes;
     public bool[] MatchAnchors;
     public Vector3[] AnchorPosition;
-    public Vector3[] AnchorRotation;
+    public Quaternion[] AnchorRotation;
     public ArticulationJointType[] ArticulationJointTypes;
+    public ArticulationDriveAxis[] ArticulationDrivesAxis;
+    private ArticulationDrive[] _articulationDrives;
     /// <summary>
-    /// 1 - X;
-    /// 2 - Y;
-    /// 3 - Z;
+    /// OBS.: Not all Articulation Body types can have Locked Motion
     /// </summary>
-    public uint[] AxisIfPrismatic;
-    /// <summary>
-    /// 1 - Free;
-    /// 2 - Limited;
-    /// </summary>
-    public uint[] MotionIfPrismaticOrRevolut;
+    public ArticulationDofLock[] MotionIfPrismaticOrRevolut;
     public uint[] SwingYIfSpherical;
     public uint[] SwingZIfSpherical;
     public uint[] TwistIfSpherical;
@@ -57,7 +46,72 @@ public class ArticulationDevice : BaseActuator
 
     public override void ConfigureDevice()
     {
-        throw new System.NotImplementedException();
+        // create new articulation bodies in array and assign their individual parents
+
+        // set type for all articulation bodies
+
+        // assign all settings if they are not null
+
+        _articulationBodies = new ArticulationBody[ChildrenArticulationBodies.Length];
+        _articulationDrives = new ArticulationDrive[ChildrenArticulationBodies.Length];
+
+        for (int i = 0; i < ChildrenArticulationBodies.Length; i++)
+        {
+            _articulationBodies[i] = ChildrenArticulationBodies[i].AddComponent<ArticulationBody>();
+            _articulationBodies[i].mass = MassPerArticulationBody[i];
+            _articulationBodies[i].useGravity = UseGravity[i];
+            _articulationBodies[i].automaticCenterOfMass = AutomaticCenterOfGravity[i];
+            _articulationBodies[i].automaticInertiaTensor = AutomaticTensor[i];
+            _articulationBodies[i].immovable = Immovable[i];
+            _articulationBodies[i].linearDamping = LinearDamping[i];
+            _articulationBodies[i].angularDamping = AngularDamping[i];
+            _articulationBodies[i].collisionDetectionMode = CollisionDetectionModes[i];
+            _articulationBodies[i].matchAnchors = MatchAnchors[i];
+            _articulationBodies[i].anchorPosition = AnchorPosition[i];
+            _articulationBodies[i].anchorRotation = AnchorRotation[i];
+            _articulationBodies[i].jointType = ArticulationJointTypes[i];
+
+            switch (ArticulationJointTypes[i])
+            {
+                case ArticulationJointType.PrismaticJoint:
+                {
+                    switch (ArticulationDrivesAxis[i])
+                    {
+                        case ArticulationDriveAxis.X:
+                        {
+                            _articulationDrives[i] = _articulationBodies[i].xDrive;
+                            break;
+                        }
+                        case ArticulationDriveAxis.Y:
+                        {
+                            _articulationDrives[i] = _articulationBodies[i].yDrive;
+                            break;
+                        }
+                        case ArticulationDriveAxis.Z:
+                        {
+                            _articulationDrives[i] = _articulationBodies[i].zDrive;
+                            break;
+                        }
+                        default:
+                        {
+                            Debug.LogError("Unrecognised Axis for Articulation Drive: " + ArticulationDrivesAxis[i]);
+                            break;
+                        }
+                    }
+                    break;
+                }
+                case ArticulationJointType.RevoluteJoint:
+                {
+                    _articulationDrives[i] = _articulationBodies[i].xDrive;
+                    break;
+                }
+                default:
+                {
+                    break;
+                }
+            }
+
+        }
     }
 
     public override bool Command<T>(T targetTorque)
